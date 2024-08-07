0b7e24f6780ec58923d3167eed2558985acac518
Flik <bassem8mohamed8@gmail.com>
2024-08-07 16:24:26 +0300

    break down page 2

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { NavComponent } from "./nav/nav.component";
 import { HeroComponent } from "./hero/hero.component";
 import { AboutComponent } from "./about/about.component";
+import { ActionsComponent } from "./actions/actions.component";
+import { PricingComponent } from "./pricing/pricing.component";
+import { EndingComponent } from "./ending/ending.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-page1',
   standalone: true,
-  imports: [NavComponent, HeroComponent, AboutComponent],
+  imports: [NavComponent, HeroComponent, AboutComponent, ActionsComponent, PricingComponent, EndingComponent, FooterComponent],
   templateUrl: './page1.component.html',
   styleUrl: './page1.component.css'
 })
--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavComponent } from "../page1/nav/nav.component";
 
 @Component({
   selector: 'app-page2',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './page2.component.html',
   styleUrl: './page2.component.css'
 })
